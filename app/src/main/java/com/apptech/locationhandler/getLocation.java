ed22c0fc0416164aec64cce5f349aab39c900578
kunallguptaaa <kunallguptaa@gmail.com>
2017-01-18 05:04:48 +0530

    minor changes

--- app/src/main/java/com/apptech/locationhandler/getLocation.java
+++ app/src/main/java/com/apptech/locationhandler/getLocation.java
@@ -15,7 +15,6 @@ import android.widget.TextView;
 
 public class getLocation extends AppCompatActivity implements LocationListener {
 
-    private static final int PERMISSION_ACCESS_COARSE_LOCATION = ;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
     protected Context context;
@@ -39,7 +38,7 @@ public class getLocation extends AppCompatActivity implements LocationListener {
             ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
                     PERMISSION_ACCESS_COARSE_LOCATION);
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 0, this);
     }
 
     @Override
